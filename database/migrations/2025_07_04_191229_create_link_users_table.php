56051b407f2ba2a488bfe01a61d87d9cb6123e15
olegbugaev <bugaev0leg@yandex.ru>
2025-07-05 15:01:00 +0200

    feat: add logic to create a link

---
+++ database/migrations/2025_07_04_191229_create_link_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('links_users', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('link_id');
+            $table->foreign('link_id')->references('id')->on('links');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('link_users');
+    }
+};
